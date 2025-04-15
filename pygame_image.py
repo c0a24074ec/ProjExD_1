fa57eefef2446e3c077810937f8a047ed05f0a0a
c0a24074ec <c0a24074ec@edu.teu.ac.jp>
2025-04-15 16:59:31 +0900

    練習１０．１

--- pygame_image.py
+++ pygame_image.py
@@ -23,6 +23,7 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])#練習９
         #screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [300, 200]) #練習４
+        kk_rct = kk_img.get_rect() #練習10.1
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習５
